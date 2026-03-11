08eee6fed18067a82030715785afb2d51c6d995f
AlPaus <paus2017alex@yandex.ru>
2026-03-11 13:02:40 +0300

    fix: 5.sh

--- lab-2/5.sh
+++ lab-2/5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 n=$1
-ps ax -o pid,etimes | awk -v limit="$n" '$2 < limit && $1 != $$ {print $1}' > killed.log
+ps ax -o pid,etimes | awk -v limit="$n" -v self="$$" '$2 < limit && $1 != self {print $1}' > killed.log
 while read -r pid; do
     kill "$pid" 2>/dev/null
 done < killed.log
